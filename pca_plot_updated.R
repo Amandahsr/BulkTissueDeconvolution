0f0df3a7ee41f0a2203908dbd41222c6ae8b1011
Amandahsr <65751896+Amandahsr@users.noreply.github.com>
2020-05-22 22:40:57 +0800

    Updated pca plot test

--- pca_plot_updated.R
+++ pca_plot_updated.R
@@ -13,6 +13,10 @@ pca.data <- as.data.frame(pca.data)
 pca_plot <- ggplot(pca.data, aes(x=PC1, y=PC2))
 pca_plot + geom_point() + xlab("PC1") + ylab("PC2") + ggtitle("PCA") + theme_bw() + theme(panel.grid=element_blank())
 
+#%variance explained by each PC
+percent_variance <- 100* pca$sdev^2/sum(pca$sdev^2)
+round(percent_variance,2)
+
 #plot %variance for each PC: scree plot
 tpm.cov <- cov(pca.data)
 tpm.eigen <- eigen(tpm.cov)
@@ -20,6 +24,9 @@ percent_variance <- tpm.eigen$values / sum(tpm.eigen$values)
 tpm.screeplot <- qplot(c(1:16), percent_variance)
 tpm.screeplot + geom_line() + xlab("PCs") + ylab("%Variance") + ggtitle("Scree Plot") + theme_bw() + theme(panel.grid=element_blank())
 
-## How many PCs are you able to find? - 16 PCs, one for each column
-## How do you determine the percentage variance explained by each PCs? -summary(pca) gives this under 'proportion of variance'
-## Can you plot %variance explained by each PCs? - scree plot
+## How many PCs are you able to find?
+#- 16 PCs, one for each column
+## How do you determine the percentage variance explained by each PCs?
+#-summary(pca), square the standard deviation then divide by the sum of all squared standard deviations
+## Can you plot %variance explained by each PCs?
+#- scree plot (?)
