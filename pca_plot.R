5a4a1d7c5268b2b0292677ca46d413684d628b50
Amandahsr <65751896+Amandahsr@users.noreply.github.com>
2020-05-23 15:32:01 +0800

    pca plot test

--- pca_plot.R
+++ pca_plot.R
@@ -4,7 +4,7 @@ library(ggplot2)
 <<<<<<< HEAD
 #read dataset
 dataset <- read_excel("~/Documents/Internships/UROPS/TPM.xlsx")
-TPM <- dataset[,-1]
+TPM <- datase[,-1]
 
 #transform into appropriate format
 =======
