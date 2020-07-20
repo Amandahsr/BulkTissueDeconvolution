78b028d2d1c5dba14d5e91eba7c9db582d78d844
Amandahsr <65751896+Amandahsr@users.noreply.github.com>
2020-07-20 16:15:41 +0800

    Delete Output.R

--- Bulk Tissue Deconvolution (Bisque)/Output.R
+++
@@ -1,8 +0,0 @@
-#This script details the output step for bulk tissue deconvolution.
-
-#Extract cell-type proportions in each sample.
-control.prop <- data.frame(control.est$bulk.props)
-
-#Calculate mean of cell-type proportions across samples.
-#Mean cell-type proportions are used as comparison with stained normal heart tissue to evaluate the accuracy of the Bisque algorithm.
-control.avgprop <- data.frame(rowMeans(control.prop))
