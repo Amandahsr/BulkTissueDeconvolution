019464528987461e70d2894aa703b9be338b575c
Amandahsr <65751896+Amandahsr@users.noreply.github.com>
2020-07-20 17:16:43 +0800

    Add files via upload

---
+++ References.R
@@ -0,0 +1,28 @@
+#This script details references and citations of publicly available datasets and softwares used in this project.
+#Bulk tissue samples and stained normal heart tissue samples used are not available for reference.
+
+##Datasets used
+######################################
+1.Single-cell RNA-seq dataset from the Tucker et. al study
+URL: https://www.ahajournals.org/doi/10.1161/CIRCULATIONAHA.119.045401
+
+##Software used:
+######################################
+1.Seurat 3.1.5 used for guided clustering
+URL: https://satijalab.org/seurat/
+
+2.DAVID 6.8 used for pathway analysis
+URL: https://david.ncifcrf.gov/
+
+3.BisqueRNA 1.0.3 used for bulk tissue deconvolution
+URL: https://cran.r-project.org/web/packages/BisqueRNA/vignettes/bisque.html
+
+4.R 3.6.3 used for codes detailed in this project
+URL: https://www.r-project.org/
+
+##References:
+######################################
+Wang, L., Yu, P., Zhou, B., Song, J., Li, Z., Zhang, M., . . . Hu, S. (2020). Single-cell reconstruction of the adult human heart during heart failure and recovery reveals the cellular landscape underlying cardiac function. Nat Cell Biol, 22(1), 108-119. doi:10.1038/s41556-019-0446-7
+Tucker, N. R., Chaffin, M., Fleming, S. J., Hall, A. W., Parsons, V. A., Bedi, K. C., Jr., . . . Ellinor, P. T. (2020). Transcriptional and Cellular Diversity of the Human Heart. Circulation. doi:10.1161/CIRCULATIONAHA.119.045401
+Jew, B., Alvarez, M., Rahmani, E., Miao, Z., Ko, A., Garske, K. M., . . . Halperin, E. (2020). Accurate estimation of cell composition in bulk expression through robust integration of single-cell information. Nat Commun, 11(1), 1971. doi:10.1038/s41467-020-15816-6
+
