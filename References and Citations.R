a71786ebcfb5aa2ab3d97b33b5255702f063a075
Amandahsr <65751896+Amandahsr@users.noreply.github.com>
2020-07-20 17:02:08 +0800

    Add files via upload

---
+++ References and Citations.R
@@ -0,0 +1,21 @@
+#This script details references and citations of publicly available datasets and softwares used in this project.
+#Bulk tissue samples and stained normal heart tissue samples used are not available for reference.
+
+##Datasets used
+######################################
+#1.Single-cell RNA-seq dataset from the Tucker et. al study
+#URL: https://www.ahajournals.org/doi/10.1161/CIRCULATIONAHA.119.045401
+
+##Software used:
+######################################
+#1.Seurat 3.1.5 used for guided clustering
+#URL: https://satijalab.org/seurat/
+
+#2.DAVID 6.8 used for pathway analysis
+#URL: https://david.ncifcrf.gov/
+
+#3.BisqueRNA 1.0.3 used for bulk tissue deconvolution
+#URL: https://cran.r-project.org/web/packages/BisqueRNA/vignettes/bisque.html
+
+#4.R 3.6.3 used for codes detailed in this project
+#URL: https://www.r-project.org/
