4d6661216e5799a764c1cb8a9b57448d42cf31d3
Amandahsr <65751896+Amandahsr@users.noreply.github.com>
2020-10-30 15:20:45 +0800

    Add files via upload

--- Guided Clustering (Seurat)/Pre-processing.R
+++ Guided Clustering (Seurat)/Pre-processing.R
@@ -5,8 +5,8 @@
 library(Seurat)
 library(Matrix)
 
-#Read normalised 10X scRNA-seq dataset. The dataset contains 287,269 cells from the 4 chambers of normal human heart samples (LV, LA, RV, RA) and 33,694 genes.
-sc.counts <- Read10X("/Volumes/Seagate Backup Plus Drive/UROPS/Datasets/Single-Cell Data")
+#Read normalised 10X scRNA-seq dataset. The scRNA-seq data contains 287,269 cells from the 4 chambers of normal human heart samples (LV, LA, RV, RA) and 33,694 genes.
+sc.counts <- Read10X("path/Single-Cell Data")
 
 #Sample selection step: For the purpose of the project, retain only cells from the LV.
 LV.cells <- sc.counts[,substr(colnames(sc.counts), 1, 2) == "LV"]
