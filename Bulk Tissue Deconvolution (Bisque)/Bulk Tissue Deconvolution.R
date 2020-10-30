40b1e6b256c54991bb8db78a93ac146cbdd2886d
Amandahsr <65751896+Amandahsr@users.noreply.github.com>
2020-10-30 16:31:30 +0800

    Add files via upload

--- Bulk Tissue Deconvolution (Bisque)/Bulk Tissue Deconvolution.R
+++ Bulk Tissue Deconvolution (Bisque)/Bulk Tissue Deconvolution.R
@@ -1,41 +1,20 @@
-#This script details the bulk tissue deconvolution step using the Bisque Reference-Based Decomposition Model.
-#Bisque requires 4 inputs for the Reference-Based Decompostion Model:
-#1. Unnormalised bulk RNA-seq data in ExpressionSet format.
-#2. Unnormalised scRNA-seq data in ExpressionSet format.
-#3. Cell-type proportions from guided clustering step.
-#4. DEGs from guided clustering step (OPTIONAL).
-
-library(data.table)
+#This script details the pre-processing of Bulk RNbulk tissue deconvolution step using the Bisque Reference-Based Decomposition Model.
+#Bisque requires 4 inputs: 1) Unnormalised bulk RNA-seq data, 2) Unnormalised scRNA-seq data, 3) Cell-type proportions from guided clustering results, 4) DEGs from guided clustering step (OPTIONAL).library(BisqueRNA)
+library(Bisque)
 library(Biobase)
-library(BisqueRNA)
+library(data.table)
 
-#Read unnormalised bulk RNA-seq dataset.
-#Dataset contains 62 bulk tissue samples with 22,474 genes. There are 33 normal heart samples and 29 DCM heart samples.
-bulk.data <- read.delim("/Volumes/Seagate Backup Plus Drive/UROPS/Datasets/Bulk Tissue Data/raw_count.txt")
+#Load the required 4 inputs. The ExpressionSet of scRNA-seq would already contain the cell-type proportions from guided clustering results.
+sc.exp <- readRDS("path/scEXP.RData")
+bulk.exp <- readRDS("path/bulkEXP.RData")
+deg.genes <- readRDS("path/ClusteringDEG.RData")
 
-#Gene IDs in bulk RNA-seq data are processed to remove decimal places for comparison with genes in scRNA-seq data.
-rownames(bulk.data) <- gsub("\\.[0-9]","", rownames(bulk.data))
-
-#Read bulk tissue sample info.
-experiment.labels <- read.table("/Volumes/Seagate Backup Plus Drive/UROPS/Datasets/Bulk Tissue Data/grouping.txt")
-
-#Retain only normal heart samples in bulk RNA-seq data.
-control.labels <- experiment.labels[experiment.labels$V2 == 'NF',]
-bulk.control <- bulk.data[,intersect(control.labels$V1, colnames(bulk.data))]
-
-#Convert bulk RNA-seq data to ExpressionSet format for bulk tissue deconvolution.
-bulk.control <- as.matrix(bulk.control)
-bulk.exp <- ExpressionSet(bulk.control)
-
-#Read DEGs from guided clustering to use as markers for bulk tissue deconvolution.
-deg.genes <- read.table("/Volumes/Seagate Backup Plus Drive/UROPS/Datasets/DEG Analysis/Unique_DEGs2.csv")
-
-#Replace DEG names with gene IDs.
+#Load gene name to gene ID matrix. Replace DEG names with gene IDs to make them comparable with gene IDs in scRNA-seq and bulk RNA-seq data.
+sc.genes <- readRDS("path/GeneNametoID.RData")
 gene.markers <- subset(sc.genes, V2 %in% deg.genes$V1)
 gene.ids <- as.vector(gene.markers$V1)
 
-#Perform bulk tissue deconvolution using the Bisque Reference-Based Decomposition Model.
-#use.overlap was set to FALSE because there are no common samples between scRNA-seq and bulk RNA-seq data.
+#Perform bulk tissue deconvolution using the Bisque Reference-Based Decomposition Model. use.overlap was set to FALSE because there are no common samples between scRNA-seq and bulk RNA-seq data.
 control.est <- BisqueRNA::ReferenceBasedDecomposition(bulk.exp, sc.exp, markers = gene.ids, use.overlap = FALSE)
 
 
