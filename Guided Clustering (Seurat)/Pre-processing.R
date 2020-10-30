6fdb311a782ade1bca423284dc94edb25db4cae7
Amandahsr <65751896+Amandahsr@users.noreply.github.com>
2020-10-30 15:08:22 +0800

    Add files via upload

--- Guided Clustering (Seurat)/Pre-processing.R
+++ Guided Clustering (Seurat)/Pre-processing.R
@@ -1,19 +1,18 @@
 #This script details the pre-processing step done on scRNA-seq before guided clustering was performed.
-
+#The scRNA-seq data used is publicly available from Tucker et. al. which can be downloaded [here](https://singlecell.broadinstitute.org/single_cell/study/SCP498/transcriptional-and-cellular-diversity-of-the-human-heart#study-download).
 library(Seurat)
 library(Matrix)
 
-#Read normalised scRNA-seq dataset.
-#Dataset contains 287,269 cells from the 4 chambers of normal human heart samples (LV, LA, RV, RA) and 33,694 genes.
+#Read normalised 10X scRNA-seq dataset. The dataset contains 287,269 cells from the 4 chambers of normal human heart samples (LV, LA, RV, RA) and 33,694 genes.
 sc.counts <- Read10X("/Volumes/Seagate Backup Plus Drive/UROPS/Datasets/Single-Cell Data")
 
-#Sample selection step:
-#Retain only cells from the LV for guided clustering.
+#Sample selection step: For the purpose of the project, retain only cells from the LV.
 LV.cells <- sc.counts[,substr(colnames(sc.counts), 1, 2) == "LV"]
 
-#Genes selection step:
-#Retain genes that meet two criteria: 
-#1) Has counts in at least 10 cells and 2) Belongs to the top 30% highly expressed genes
+#Pre-process genes to ensure we only cluster genes with meaningful data. Retain genes that meet two criteria: 
+#1) Has counts in at least 10 cells and 2) Belongs to the top 30% highly expressed genes.
 LV.cells <- LV.cells[rowSums(LV.cells != 0) >= 10, ,drop = FALSE]
+
+#Due to RAM limitations, only retain the top 30% highly-expressed genes.  
 LV.cells <- LV.cells[rev(order(rowSums(LV.cells))),]
 LV.cells <- LV.cells[1:7494,]
